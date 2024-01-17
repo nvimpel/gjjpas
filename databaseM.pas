34bcd87fa147bb4467e79e964c1bfac5e291e36f
Noel Vimpel <71408730+nvimpel@users.noreply.github.com>
2024-01-17 23:25:45 +0100

    Update databaseM.pas

--- databaseM.pas
+++ databaseM.pas
@@ -13,12 +13,13 @@ Tsubor = file of TClovek;
 var opt:char;
     clovek:TClovek;
     subor,tempSubor:Tsubor;
-
+{Vypis hlavicky tabulky}
 procedure Hlavicka();
 begin
     clrscr;
     writeln('RODNE C.':11, 'MENO':11, 'PRIEZVISKO':21, 'PLAT':6, 'POHLAVIE':9);
 end;
+{Vypis jedneho cloveka}
 procedure VypisCloveka(clovek:TClovek);
 begin
     write(clovek.rc:11, clovek.meno:11, clovek.pr:21, clovek.plat:6);
@@ -29,34 +30,40 @@ end;
 function numberTest(rc:string):boolean;
 var even, odd:integer;
     i,n:byte;
-    localTest:boolean;
+    localNumberTest:boolean;
 begin
     even := 0;
     odd := 0;
-    localTest := false;
+    localNumberTest := false;
+    {Spocitaj cisla na parnych a neparnych miestach individualne}
     for i := 1 to 10 do begin
         if i mod 2 = 0 then even := even + ord(rc[i]) - 48
         else odd := odd + ord(rc[i]) - 48;
     end;
     n := abs(odd - even);
-    if ((n mod 11 = 0) or (n = 0)) then localTest := true;
-    numberTest := localTest;
+    {Rozdiel musi byt delitelny 11 - pravidlo rodnych cisel}
+    if ((n mod 11 = 0) or (n = 0)) then localNumberTest := true;
+    numberTest := localNumberTest;
 end;
 {Funkcia na testovanie rodneho cisla}
 function testRC(rc:string):boolean;
-var duo:string[2];
-    valDuo:integer;
+var duo:string[2]; {dvojica cisel}
+    valDuo:integer; {dvojica cisel premenena na integer}
     err:byte;
     localTestRC:boolean;
 begin
     localTestRC := true;
+    {Testuj dlzku rodneho cisla}
     if length(rc) <> 10 then localTestRC := false;
     duo := copy(rc, 3, 2);
     val(duo, valDuo,err);
+    {skontroluj mesiac}
     if not(((valDuo >= 1) and (valDuo <= 12)) or ((valDuo >= 51) and (valDuo <= 62))) then localTestRC := false;
     duo := copy(rc, 5, 2);
     val(duo, valDuo,err);
+    {skontroluj den}
     if not((valDuo >= 1) and (valDuo <= 31)) then localTestRC := false;
+    {skontroluj pravidlo delitelnosti 11 - funkcia numberTest} 
     if numberTest(rc) = false then localTestRC := false;
     testRC := localTestRC;
 end;
@@ -70,18 +77,17 @@ begin
     writeln('5. Vypis databazu');
     writeln('6. Ukonci program');
 end;
+{Procedure na vypis menu pre upravu udajov}
 procedure menuUprav();
 begin
     writeln('1. Zmen rodne cislo');
     writeln('2. Zmen meno');
     writeln('3. Zmen priezvisko');
     writeln('4. Zmen plat');
-    writeln('5. Zmen pohlavie');
 end;
 {Procedure na vlozenie cloveka do databazy}
 procedure Vloz();
 var vklad:TClovek;
-    sex:char;
 begin
     seek(subor, filesize(subor));
     repeat
@@ -95,14 +101,10 @@ begin
     readln(vklad.pr);
     write('Zadaj plat: ');
     readln(vklad.plat);
-    repeat
-        clrscr;
-        write('Zadaj pohlavie (M/Z): ');
-        readln(sex);
-    until ((sex = 'M') or (sex = 'Z'));
-    if sex = 'M' then vklad.sex := true else vklad.sex := false;
+    if ((vklad.rc[3] = '0') or (vklad.rc[3] = '1')) then vklad.sex := true else vklad.sex := false; 
     write(subor, vklad);
 end;
+{Procedure na hladanie cloveka podla rodneho cisla, ID je pozicia v subore}
 procedure Hladam(rc:string; var ID:longint);
 var found:boolean;
     clovek:TClovek;
@@ -119,6 +121,7 @@ begin
         VypisCloveka(clovek);
     end else begin writeln('Clovek s tymto rodnym cislom neexistuje'); ID := -1; end; 
 end;
+{Procedure na hladanie cloveka podla rodneho cisla, err je pozicia v subore ak error = -1 tak clovek neexistuje}
 procedure Najdi();
 var rc:string[10];
     err:longint;
@@ -128,11 +131,11 @@ begin
     Hladam(rc,err);
     readln;
 end;
+{Procedure na opravu udajov cloveka, err je pozicia v subore ak error = -1 tak clovek neexistuje}
 procedure Oprav();
 var rc:string;
     opt:char;
     err:longint;
-    sex:char;
     clovek:TClovek;
 begin
     write('Zadaj rodne cislo cloveka ktoreho chces opravit: ');
@@ -151,6 +154,7 @@ begin
                     write('Zadaj rodne cislo bez lomitka: ');
                     readln(clovek.rc);
                 until testRC(clovek.rc) = true;
+                if ((clovek.rc[3] = '0') or (clovek.rc[3] = '1')) then clovek.sex := true else clovek.sex := false; 
             end;
             '2':begin
                 write('Zadaj krsne meno: ');
@@ -164,19 +168,12 @@ begin
                 write('Zadaj plat: ');
                 readln(clovek.plat);
             end;
-            '5':begin
-                repeat
-                    clrscr;
-                    write('Zadaj pohlavie (M/Z): ');
-                    readln(sex);
-                until ((sex = 'M') or (sex = 'Z'));
-                if sex = 'M' then clovek.sex := true else clovek.sex := false;
-            end;
         end;
         seek(subor, err);
         write(subor, clovek);
     end;
 end;
+{Procedure na vymazanie cloveka z databazy, err je pozicia v subore ak error = -1 tak clovek neexistuje}
 procedure Vymaz();
 var rc:string;
     err:longint;
@@ -201,14 +198,26 @@ begin
         end;
     end;
 end;
+{Procedure na vypis databazy}
 procedure Vypis();
 var clovek:TClovek;
+    size,pages,page:longint;
+    i:byte;
+
 begin
     reset(subor);
+    size := filesize(subor);
+    if ((size div 10 = 0) and (size > 10)) then pages := size div 10 else pages := (size div 10) + 1;
+    writeln('Databaza obsahuje ', size, ' zaznamov a ', pages, ' stran po 10 zaznamov');
+    writeln('Zadaj cislo strany ktoru chces vypisat: ');
+    readln(page);
     Hlavicka();
-    while not(eof(subor)) do begin
-        read(subor, clovek);
-        VypisCloveka(clovek);
+    for i := 1 to 10 do begin
+        if not(eof(subor)) then begin
+            seek(subor, (page-1)*10 + i);
+            read(subor, clovek);
+            VypisCloveka(clovek);
+        end;
     end;
     readln;
 end;
