b19ebc3cbd229ef96abd28372d0c45795cd019a2
Noel Vimpel <71408730+nvimpel@users.noreply.github.com>
2024-01-20 12:20:51 +0100

    Add files via upload

---
+++ txtToDat.pas
@@ -0,0 +1,26 @@
+program convert;
+
+uses crt;
+var fileIn:text;
+    fileOut:file of string[15];
+    s:string[15];
+    nameIn,nameOut:string;
+begin
+    readln(nameIn);
+    nameOut:=nameIn+'.dat';
+    nameIn:=nameIn+'.txt';
+
+    assign(fileIn,nameIn);
+    assign(fileOut,nameOut);
+    reset(fileIn);
+    rewrite(fileOut);
+
+    while not eof(fileIn) do begin
+        readln(fileIn,s);
+        write(fileOut,s);
+    end;
+
+    close(fileIn);
+    close(fileOut);
+
+end.
