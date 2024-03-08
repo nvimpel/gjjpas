d1c1a35f01e2009549b1550f400f7eeccbdaa004
Noel Vimpel <71408730+nvimpel@users.noreply.github.com>
2024-03-08 15:42:35 +0100

    Add files via upload

---
+++ otacac.pas
@@ -0,0 +1,39 @@
+program otacac;
+uses crt;
+
+var subIn,subOut:text;
+    S:string;
+
+function otoc(slovo:string):string;
+var i,l:byte;
+    tempS:string;
+
+begin
+    l := length(slovo);
+    tempS[0]:=chr(l);
+    for i:=1 to l do begin
+        tempS[i]:=slovo[l-i+1];
+    end;
+    otoc:=tempS;
+end;
+
+begin
+    clrscr;
+    assign(subIn, 'slovaO.txt');
+    {assign(subOut, 'slovaO.txt');}
+    reset(subIn);
+    {rewrite(subOut);}
+    {while(not(eof(subIn))) do begin
+        readln(subIn, S);
+        writeln(S);
+    end;}
+    reset(subIn);
+    while(not(eof(subIn))) do begin
+        readln(subIn, S);
+        writeln(otoc(S));
+    end; 
+    readln();
+    close(subIn);
+    {close(subOut);}
+
+end.
---
+++ pr1502.pas
@@ -0,0 +1,27 @@
+program test;
+uses crt;
+
+var subIn:text;
+    subOut:text;
+    n,i,j:byte;
+
+begin
+    clrscr;
+    assign(subIn, 'tempIn.txt');
+    assign(subOut, 'tempOut.txt');
+    reset(subIn);
+    rewrite(subOut);
+    while not(eof(subIn)) do begin
+        readln(subIn, n);
+        for i:=1 to n do begin
+            for j:=1 to n do begin 
+                if ((i=2)or(i=n-1)or((j=2) or (j=n-1))) and not((i=1)or(i=n)or(j=1)or(j=n))then write(subOut, ' ')
+                else write(subOut, '*');
+            end;
+            writeln(subOut, '');
+        end;
+        writeln(subOut, '');
+    end;
+    close(subIn);
+    close(subOut);
+end.
